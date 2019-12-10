b907055a060c5c857d2d44b8d51fe4205b1da965
wzb <wzb3422@gmail.com>
2019-12-10 16:46:33 +0800

    :construction: Working on horizontal scroll

--- src/pages/home/HomeHeader.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HomeHeader: React.FC = () => {
-  return (
-    <div>HomeHeader</div>
-  )
-}
-
-export default HomeHeader;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import './index.scss';
+
+const HomeHeader = lazy(() => import('./HomeHeader'));
 
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
-      <Link to='/team'>
-        Go to team
-      </Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className='home-content-limit'>
+          <HomeHeader />
+          <div className='scroll-content'>
+            <div className='scroll-content-item'></div>
+            <div className='scroll-content-item'></div>
+            <div className='scroll-content-item'></div>
+          </div>
+        </div>
+      </Suspense>
     </>
   )
 }
