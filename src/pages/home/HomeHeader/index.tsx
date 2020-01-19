d08b59e9247b7764d4dc5fb3c5a562e137d05c4b
wzb <wzb3422@gmail.com>
2020-01-19 17:28:32 +0800

    :art: Remove visibility

--- src/pages/home/HomeHeader/index.tsx
+++ src/pages/home/HomeHeader/index.tsx
@@ -3,13 +3,9 @@ import './style';
 
 const headerItems = ['首页', '产品', '博客', '团队'];
 
-interface Props {
-  visible: boolean;
-}
+const HomeHeader: React.FC = () => {
 
-const HomeHeader: React.FC<Props> = ({ visible }) => {
-
-  const [barOffsetX, setBarOffsetX] = useState<number>(0)
+  const [barOffsetX, setBarOffsetX] = useState<number>(0);
 
   const changeBarOffset = (index: number): void => {
     if (index !== 0) {
@@ -23,29 +19,25 @@ const HomeHeader: React.FC<Props> = ({ visible }) => {
     <div
       className='home-header'
     >
-      {visible ? (
-        <>
-          <ul>
-            {headerItems.map((item, index) => {
-              return (
-                <li
-                  key={item}
-                  onMouseOver={() => {changeBarOffset(index)}}
-                  onMouseLeave={() => {changeBarOffset(0)}}
-                >
-                  {item}
-                </li>
-              )
-            })}
-            <li>加入我们</li>
-          </ul>
-          <div
-            className='home-header-bar'
-            style={{transform: `translateX(${barOffsetX}px)`}}
-          >
-          </div>
-        </>
-      ) : null}
+      <ul>
+        {headerItems.map((item, index) => {
+          return (
+            <li
+              key={item}
+              onMouseOver={() => {changeBarOffset(index)}}
+              onMouseLeave={() => {changeBarOffset(0)}}
+            >
+              {item}
+            </li>
+          )
+        })}
+        <li>加入我们</li>
+      </ul>
+      <div
+        className='home-header-bar'
+        style={{transform: `translateX(${barOffsetX}px)`}}
+      >
+      </div>
     </div>
   )
 }
