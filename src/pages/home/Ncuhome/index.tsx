63cfecf091f1fac208b3f8d59729be6e59b4cde4
wzb <wzb3422@gmail.com>
2020-01-19 17:29:07 +0800

    :construction: Working on scroll

---
+++ src/pages/home/Ncuhome/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style'; 
+
+const Ncuhome: React.FC = () => {
+  return (
+    <div className='ncuhome-wrapper'>Ncuhome</div>
+  )
+}
+
+export default Ncuhome;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,42 +1,20 @@
 import React, { Suspense, lazy, useEffect, useState } from 'react';
 import './index.scss';
+import HorizontalScroll from 'Src/components/HorizontalScroll';
 
-const HomeHeader = lazy(() => import('./HomeHeader'));
-const Introduction = lazy(() => import('./Introduction'));
+const Ncuhome = lazy(() => import('./Ncuhome'));
 const Products = lazy(() => import('./Products'));
 
 const Home = () => {
 
-  const [scrollX, setScrollX] = useState<number>(0);
-  const [headerVisible, setHeaderVisible] = useState<boolean>(true);
-
-  // 监听滚动
-  useEffect(() => {
-    window.onscroll = () => {
-      setScrollX(document.body.scrollTop);
-      if (scrollX > 276 && headerVisible === true) {
-        setHeaderVisible(false);
-      }
-      if (scrollX <= 276 && headerVisible === false) {
-        setHeaderVisible(true);
-      }
-    }
-  });
-
   return (
     <>
       <Suspense fallback={<div>Loading...</div>}>
-        <div className='home-content-limit'>
-          <HomeHeader visible={headerVisible}/>
-          <div
-            className='scroll-content'
-            style={{transform: `translateX(-${scrollX}px)`}}
-          >
-            <Introduction />
-            <Products />
-            <div className='scroll-content-item'></div>
-          </div>
-        </div>
+        <HorizontalScroll>
+          <Ncuhome />
+          <Ncuhome />
+          {/* <Products /> */}
+        </HorizontalScroll>
       </Suspense>
     </>
   )
